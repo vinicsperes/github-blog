c7ef98da158e58195670a62c3db2fa2bb8876f44
ohperes <peresvini2@gmail.com>
2023-01-14 19:02:14 -0300

    feat: consume user data from github API

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -15,38 +15,60 @@ import {
 } from '@fortawesome/free-solid-svg-icons'
 import { PostCard } from '../../components/PostCard'
 
+import { useEffect, useState } from 'react'
+
+interface githubDataType {
+  avatar_url?: string
+  bio?: string
+  company?: string
+  followers?: number
+  login?: string
+  name?: string
+  githubUrl?: string
+}
+
 export function Blog() {
+  const [githubData, setGithubData] = useState<githubDataType>({})
+
+  const fetchData = () => {
+    return fetch(`https://api.github.com/users/vinicsperes`)
+      .then((response) => response.json())
+      .then((data) => setGithubData(data))
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  console.log(githubData)
+
   return (
     <BlogContent>
       <Profile>
-        <ProfileImg src="https://github.com/ohperes.png" alt="Foto de perfil" />
+        <ProfileImg src={githubData.avatar_url} alt="Foto de perfil" />
         <ProfileContent>
           <div>
-            <p className="titleL">Cameron Williamsons</p>
+            <p className="titleL">{githubData.name}</p>
             <span className="link">
               <p>GITHUB</p>
               <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
             </span>
           </div>
 
-          <p className="textM">
-            Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
-            viverra massa quam dignissim aenean malesuada suscipit. Nunc,
-            volutpat pulvinar vel mass.
-          </p>
+          <p className="textM">{githubData.bio}</p>
 
           <PersonalInfo className="textM">
             <span>
               <FontAwesomeIcon icon={faGithub} />
-              <p>ohperes</p>
+              <p>{githubData.login}</p>
             </span>
             <span>
               <FontAwesomeIcon icon={faBuilding} />
-              <p>Estudando</p>
+              <p>{githubData.company}</p>
             </span>
             <span>
               <FontAwesomeIcon icon={faUserGroup} />
-              <p>20 seguidores</p>
+              <p>{githubData.followers} followers</p>
             </span>
           </PersonalInfo>
         </ProfileContent>
