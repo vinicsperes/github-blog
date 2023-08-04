4d32fbfbf21e154dfe7eb0bbf85f59227df249bc
vinicsperes <peresvini2@gmail.com>
2023-08-03 21:13:33 -0300

    Remove comments

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -101,7 +101,6 @@ export default function Post() {
       <PostContent>
         <ReactMarkdown>{postInfo.body}</ReactMarkdown>
       </PostContent>
-      {/* Other content for the individual post */}
     </PostMain>
   )
 }
